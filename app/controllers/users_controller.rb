8665b5af31981c985d97be61cf420c850e7f093b
Yusuf Demir <yusufdemir286634@gmail.com>
2021-01-29 13:23:19 +0300

    Register Tamamlandı

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    # Kayıt Ol
+    # Girdi - email, password
+    # Çıktı - user
+    def create
+      existing_user = User.find_by(email: params[:email])
+      if existing_user
+        render json: {error: "Kullanıcı Zaten Kayıtlı."}
+      else
+        @user = User.create(user_params)
+        if @user.valid?
+          render json: { user: @user }
+        else
+          render json: {error: "Geçersiz İşlem."}
+        end
+      end
+      
+    end
+
+    private
+    def user_params
+      params.permit(:name, :surname, :email, :password)
+    end
+  
 end
