eb3f84a8ab001214fca1e040f683286f92760a36
Yusuf Demir <yusufdemir286634@gmail.com>
2021-01-29 13:51:38 +0300

    Refresh Token, Access Token adında iki kolon Users tablosuna eklendi.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_102624) do
+ActiveRecord::Schema.define(version: 2021_01_29_105101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_01_29_102624) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "refresh_token"
+    t.string "access_token"
   end
 
 end
